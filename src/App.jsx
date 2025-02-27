f3898a0c8a934f5f59afc4638f945ed727edb90c
mohammedmeski200 <mohammedmeski200@gmail.com>
2025-02-27 10:06:25 +0000

    added tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-function ProductList() {
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        axios.get("http://localhost:8000/api/products")
-            .then(response => setProducts(response.data))
-            .catch(error => console.error(error));
-    }, []);
+export default function App() {
 
     return (
-        <div>
-            <h1>Products</h1>
-            <ul>
-                {products.map(product => (
-                    <li key={product.id}>{product.name} - {product.price}</li>
-                ))}
-            </ul>
-        </div>
+        <h1>
+            hello  world
+        </h1>
     );
 }
-
-export default ProductList;
